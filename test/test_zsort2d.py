796ae88f6510a41437f4247976d10ceb1689349a
Jürgen Weigert <jw@owncloud.com>
2019-05-11 23:47:29 +0200

    proper 2d rotate and sort done.

--- test/test_zsort2d.py
+++ test/test_zsort2d.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python
 #
 # Zsort is only done for the rim.
-# - the general 3d face sorting problem can be reduced to a 2d problem as all faces span between two parallel planes.
-# - Each quad-face can be represented by a two-point line in 2d.
-# - We need to find a 2d rotation that so that the eye vector is exactly downwards in 2d.
+# - the general 3d face sorting problem can be reduced to a 2D problem as all faces span between two parallel planes.
+# - Each quad-face can be represented by a two-point line in 2D.
+# - We need to find a 2D rotation that so that the eye vector is exactly downwards in 2D.
 # - rotate all faces.
 # - comparison:
 #   test all 4 endpoints:
@@ -25,6 +25,7 @@
 
 from __future__ import print_function
 import functools
+import numpy as np
 
 test_zsort = [
   ((1,1), (5,3), "near" ),
@@ -33,7 +34,6 @@ test_zsort = [
 ]
 
 eps = 1e-100
-
 
 def y_at_x(gp, gv, x):
   dx = x-gp[0]
@@ -45,7 +45,7 @@ def y_at_x(gp, gv, x):
   return gp[1]+s*gv[1]
 
 
-def cmp2d(g1, g2):
+def cmp2D(g1, g2):
   """
   returns -1 if g1 sorts in front of g2
   returns 1  if g1 sorts in behind g2
@@ -77,9 +77,97 @@ def cmp2d(g1, g2):
     if g1[1][1]-eps < y: return -1
     if g1[1][1]+eps > y: return 1
   #
-  return 0
+  # non-overlapping. keep the index order.
+  if g1[2] == g2[2]: return 0
+  if g1[2] <  g2[2]: return -1
+  return 1
 
-k = functools.cmp_to_key(cmp2d)
 
-print(sorted(test_zsort, key=k))
+def genRx(theta):
+  "A rotation matrix about the X axis. Example: Rx = genRx(np.radians(30))"
+  c, s = np.cos(theta), np.sin(theta) 
+  return np.array( ((1, 0, 0), (0, c, s), (0, -s, c)) )
+
+
+def genRy(theta):
+  "A rotation matrix about the Y axis. Example: Ry = genRy(np.radians(30))"
+  c, s = np.cos(theta), np.sin(theta)
+  return np.array( ((c, 0, -s), (0, 1, 0), (s, 0, c)) )
+
+
+def genRz2D(theta):
+  "A 2D rotation matrix about the Z axis. Example: Rz2D = genRz2D(np.radians(30))"
+  c, s = np.cos(theta), np.sin(theta)
+  return np.array( ((c, s), (-s, c)) )
+
+
+def phi2D(R):
+  """
+  Given a 3D rotation matrix R, we compute the angle phi projected in the 
+  x-y plane of point 0,0,1 relative to the negative Y axis.
+  """
+  (x2d_vec, y2d_vec, dummy) = np.matmul( [0,0,-1], R )
+  if abs(x2d_vec) < eps:
+    if abs(y2d_vec) < eps: return 0.0
+    phi = 0.5*np.pi
+    if y2d_vec < 0:
+      phi = -0.5*np.pi
+    else:
+      phi = 0.5*np.pi
+  else:
+    phi = np.arctan(y2d_vec/x2d_vec)
+  if x2d_vec < 0:       # adjustment for quadrant II and III
+    phi += np.pi
+  elif y2d_vec < 0:     # adjustment for quadrant IV
+    phi += 2*np.pi
+  phi += 0.5*np.pi      # adjustment for starting with 0 deg at neg Y-axis.
+  if phi >= 2*np.pi:
+    phi -= 2*np.pi      # adjustment to remain within 0..359.9999 deg
+  return phi
+
+
+def dphi2D(x2d_vec, y2d_vec):
+  """ testing code only """
+  if abs(x2d_vec) < eps:
+    if abs(y2d_vec) < eps: return 0.0
+    if y2d_vec < 0:
+      phi = -0.5*np.pi
+    else:
+      phi = 0.5*np.pi
+  else:
+    phi = np.arctan(y2d_vec/x2d_vec)
+  if x2d_vec < 0:       # adjustment for quadrant II and III
+    phi += np.pi
+  elif y2d_vec < 0:     # adjustment for quadrant IV
+    phi += 2*np.pi
+  phi += 0.5*np.pi      # adjustment for starting with 0 deg at neg Y-axis.
+  if phi >= 2*np.pi:
+    phi -= 2*np.pi      # adjustment to remain within 0..359.9999 deg
+  return np.degrees(phi)
+
+
+# Dimetric 7,42 transformation
+Ry = genRy(np.radians(69.7)) 
+Rx = genRx(np.radians(19.4))
+
+## Isometric transformation
+# Ry = genRy(np.radians(45)) 
+# Rx = genRx(np.radians(35.26439))
+
+R = np.matmul(Ry, Rx)
+
+# prepare a rotated version of the original two-D line set,
+# so that cmp2D can sort towards negaive Y-Axis
+Rz2D = genRz2D(phi2D(R))
+rotated = []
+for i in range(len(test_zsort)):
+  l = test_zsort[i]
+  rotated.append((np.matmul(l[0], Rz2D), np.matmul(l[1], Rz2D), i)+l[2:])
+
+k = functools.cmp_to_key(cmp2D)
+
+print(test_zsort)
+print(np.degrees(phi2D(R)))
+print(rotated, "\n\n")
+print(sorted(rotated, key=k))
 
